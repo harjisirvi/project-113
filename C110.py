7ed4a3caa34685c1fbb7804e39e7407a449207ac
harjisirvi <92523298+harjisirvi@users.noreply.github.com>
2022-11-24 20:50:50 +0530

    Add files via upload

---
+++ C110.py
@@ -0,0 +1,108 @@
+#name = input("enter the name")
+#print (name)
+
+#import time 
+#print(dir(time))
+
+#seconds=input("enter the seconds")
+#def timer(seconds):
+ #   while seconds>0:
+  #      mins = int(seconds / 60)
+   #     secs = int(seconds % 60)
+    #    timer = f'{mins}:{secs}'
+     #   print(timer)
+      #  time.sleep(1)
+       # seconds -=1
+
+#timer(int(seconds))
+
+import random
+x = "y"
+while x == "y":
+	no = random.randint(1,6)
+	if no == 1:
+		print("[-----]")
+		print("[	 ]")
+		print("[  0  ]")
+		print("[	 ]")
+		print("[-----]")
+	if no == 2:
+		print("[-----]")
+		print("[ 0 ]")
+		print("[	 ]")
+		print("[ 0 ]")
+		print("[-----]")
+	if no == 3:
+		print("[-----]")
+		print("[	 ]")
+		print("[0 0 0]")
+		print("[	 ]")
+		print("[-----]")
+	if no == 4:
+		print("[-----]")
+		print("[0 0]")
+		print("[	 ]")
+		print("[0 0]")
+		print("[-----]")
+	if no == 5:
+		print("[-----]")
+		print("[0 0]")
+		print("[ 0 ]")
+		print("[0 0]")
+		print("[-----]")
+	if no == 6:
+		print("[-----]")
+		print("[0 0 0]")
+		print("[	 ]")
+		print("[0 0 0]")
+		print("[-----]")	
+	x=input("press y to roll again and n to exit:")
+
+	print("\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
